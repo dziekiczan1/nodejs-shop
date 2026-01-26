3a633d68644c9d54f114c743c254402811b3cdf0
dzekiczan1 <p.rzadkowolski@gmail.com>
2026-01-26 17:15:53 +0100

    feat(db): add Sequelize and switch to DB-based fetch

--- util/db.js
+++ util/db.js
@@ -1,11 +1,13 @@
-require("dotenv").config();
-const mysql = require("mysql2");
+const Sequalize = require("sequelize");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-});
+const sequelize = new Sequalize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+  },
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;
