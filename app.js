63b0088b150d29531914342f30a55eb748692855
dzekiczan1 <p.rzadkowolski@gmail.com>
2026-01-27 20:21:24 +0100

    feat(cart): add CartItem model, update Cart, improve cart views and styles

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const errorController = require("./controllers/error");
 const sequelize = require("./util/db");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -38,6 +40,10 @@ app.use(errorController.get404);
 // Define model relationships
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   // .sync({ force: true })
@@ -52,6 +58,9 @@ sequelize
     return user;
   })
   .then((user) => {
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => {
