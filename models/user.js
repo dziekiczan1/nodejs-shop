582473a4f7cc32f0453ba6ffadebe3dad8cfc496
dzekiczan1 <p.rzadkowolski@gmail.com>
2026-02-02 18:55:58 +0100

    feat(auth): add password reset and user authorization for product

--- models/user.js
+++ models/user.js
@@ -157,6 +157,12 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  resetToken: {
+    type: String,
+  },
+  resetTokenExpiration: {
+    type: Date,
+  },
   cart: {
     items: [
       {
