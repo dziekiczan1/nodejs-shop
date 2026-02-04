93f1266bcb6da7cb221eb2efa97fa104771d67b3
dzekiczan1 <p.rzadkowolski@gmail.com>
2026-02-04 19:10:10 +0100

    feat(orders): generate PDF invoices for orders

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const fileHelper = require("../util/file");
 
 const Product = require("../models/product");
 
@@ -289,6 +290,7 @@ exports.postEditProduct = (req, res, next) => {
       product.price = updatedPrice;
       product.description = updatedDescription;
       if (image) {
+        fileHelper.deleteFile(product.imageUrl);
         product.imageUrl = image.path;
       }
       return product.save().then((result) => {
@@ -305,12 +307,38 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteOne({
-    _id: prodId,
-    userId: req.user._id,
-  })
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return next(new Error("Product not found."));
+      }
+      return Product.deleteOne({
+        _id: prodId,
+        userId: req.user._id,
+      });
+    })
     .then(() => {
       console.log("Deleted Product");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return next(new Error("Product not found."));
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
+    .then(() => {
+      console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
     })
     .catch((err) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,3 @@
-const path = require("path");
-
 const express = require("express");
 
 const shopController = require("../controllers/shop");
@@ -13,15 +11,17 @@ router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
 
 router.get("/products/:productId", shopController.getProduct);
-//
+
 router.get("/cart", isAuth, shopController.getCart);
-//
+
 router.post("/cart", isAuth, shopController.postCart);
-//
+
 router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
-//
+
 router.post("/create-order", isAuth, shopController.postOrder);
-//
+
 router.get("/orders", isAuth, shopController.getOrders);
+
+router.get("/orders/:orderId", isAuth, shopController.getInvoice);
 
 module.exports = router;
